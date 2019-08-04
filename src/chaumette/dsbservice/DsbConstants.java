bb0e4b548e20f87d7e0aa6ec7a58f8004d67f7b4
Scroogeee <30441849+Scroogeee@users.noreply.github.com>
2019-08-04 09:20:16 +0200

    Update DsbConstants.java

--- src/chaumette/dsbservice/DsbConstants.java
+++ src/chaumette/dsbservice/DsbConstants.java
@@ -5,12 +5,12 @@ public abstract class DsbConstants {
 	public static final String loginFormUrl = "https://www.dsbmobile.de/Login.aspx?logout";
 	public static final String loginActionUrl = "https://www.dsbmobile.de/Login.aspx?logout";
 	public static final String cleanPageBaseString = "https://app.dsbcontrol.de/data/ec81efa5-fc68-44d6-b18c-c27839832c23";
-	public static final String username = "144501";
-	public static final String password = "kathi";
+	public static final String username = "user";
+	public static final String password = "pass";
 	public static final String userform = "txtUser";
 	public static final String passform = "txtPass";
 	public static final String submitform = "ctl03";
 
-	public static final String klasseJacques = "9c";
+	public static final String klasseJ = "";
 
 }
